ab825dc1fe542fe7e6c8a6b45ec90125ebb4cc14
Kydeeh <volkada1128@gmail.com>
2022-07-15 01:35:07 +0800

    no message

--- src/main/java/idv/App.java
+++ src/main/java/idv/App.java
@@ -11,7 +11,6 @@ public class App {
 		System.out.println("新增branch-0715");
 
 		System.out.println("user1新增branch-0715");
-		System.out.println("user2修改共同檔案...");
 		System.out.println("user1修改");
 	}
 }
